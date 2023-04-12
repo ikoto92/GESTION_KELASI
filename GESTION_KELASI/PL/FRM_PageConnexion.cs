26018d0509aaf7a471ab5376e312211c1e3fcacb
Parfait André <Parfait Andr@DESKTOP-2717K4D>
2023-04-12 14:11:27 +0100

    12-04-2023-1

--- GESTION_KELASI/PL/FRM_PageConnexion.cs
+++ GESTION_KELASI/PL/FRM_PageConnexion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ namespace GESTION_KELASI.PL
 {
     public partial class FRM_PageConnexion : Form
     {
+        SqlConnection com = new SqlConnection("Data source=DESKTOP-2717K4D\\SQLEXPRESS; initial catalog=GestionKelasi; integrated security =true ");
+        SqlDataAdapter singh = new SqlDataAdapter();
+        SqlDataReader dr;
         public FRM_PageConnexion()
         {
             InitializeComponent();
